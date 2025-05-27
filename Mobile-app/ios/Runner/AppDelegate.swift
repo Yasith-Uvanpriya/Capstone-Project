a5451d0bf636df820835553cac3181295cf809bd
HashenWatawala <hashanewatawala@gmail.com>
2025-05-27 10:58:11 +0530

    Add files with backend

--- Mobile-app/ios/Runner/AppDelegate.swift
+++ Mobile-app/ios/Runner/AppDelegate.swift
@@ -1,13 +1,16 @@
-import Flutter
 import UIKit
+import Flutter
+import GoogleMaps
 
-@main
+@UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyDCmTcfOw6OkEHi6ZyPfAR1Bc2JJEC6OSw")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
+
